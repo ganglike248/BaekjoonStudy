34d8630a019a6713a97077d287e44480fc7bdf7c
ganglike <81566899+ganglike248@users.noreply.github.com>
2026-04-05 15:00:59 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 52288 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+N = int(input())
+
+arr1 = set(map(int, input().split()))
+
+M = int(input())
+
+arr2 = list(map(int, input().split()))
+
+for i in arr2:
+    if i in arr1:
+        print("1")
+    else:
+        print("0")
